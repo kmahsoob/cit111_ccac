f680f678399d6cfdc0089dc38781d83403643a3f
kaden.mahsoob <kaden.mahsoob@acd.ccac.edu>
2018-05-15 03:55:26 -0400

    Create animals.java

---
+++ animals.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+/**
+ *
+ * @author kadenmahsoob
+ */
+public class animals {
+    public String name; 
+    public int age; 
+    public String sex;
+    public int weight;
+    //variables 
+
+    public String getName(){
+        return name; 
+    }//close method 1
+    public int getAge() {
+        return age;
+    }//close method 2
+    public String getSex() {
+        return sex; 
+    }//close method 3
+    public int getWeight() {
+        return weight; 
+    }//close method 4
+}// close class 
